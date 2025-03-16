b722c0567444289688b794b889a728fff42668ed
omar tarik yehya <khalaf.omar20@gmail.com>
2025-03-16 10:29:15 +0200

    feat: routing and navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,6 +4,7 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-dark-200 font-bold">Welcome !</Text>
+
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,16 @@
 import { Stack } from "expo-router";
 import './globals.css';
+import { StackRouter } from "@react-navigation/native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+    <Stack.Screen 
+      name="(tabs)"
+      options={{headerShown: false}}
+    />
+    <Stack.Screen 
+      name="movie/[id]"
+      options={{headerShown: false}}
+    />
+  </Stack>;
 }
