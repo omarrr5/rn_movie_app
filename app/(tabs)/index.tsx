b49afcd145e3f6b6a3db4c800ce3e58a4f38f267
omar tarik yehya <khalaf.omar20@gmail.com>
2025-03-19 11:28:18 +0200

    feat: show trending movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,32 @@
-import { icons } from "@/constants/icons";
-import { images } from "@/constants/images";
 import {
-  Text,
   View,
-  Image,
-  ScrollView,
+  Text,
   ActivityIndicator,
+  ScrollView,
+  Image,
   FlatList,
 } from "react-native";
-import SearchBar from "@/components/SearchBar";
 import { useRouter } from "expo-router";
+
 import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
-import MovieCard from "@/components/MovieCard";
+import { getTrendingMovies } from "@/services/appwrite";
 
-export default function Index() {
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+
+import SearchBar from "@/components/SearchBar";
+import MovieCard from "@/components/MovieCard";
+import TrendingCard from "@/components/TrendingCard";
+
+const Index = () => {
   const router = useRouter();
+
+  const {
+    data: trendingMovies,
+    loading: trendingLoading,
+    error: trendingError,
+  } = useFetch(getTrendingMovies);
 
   const {
     data: movies,
@@ -25,7 +36,12 @@ export default function Index() {
 
   return (
     <View className="flex-1 bg-primary">
-      <Image source={images.bg} className="absolute w-full z-0" />
+      <Image
+        source={images.bg}
+        className="absolute w-full z-0"
+        resizeMode="cover"
+      />
+
       <ScrollView
         className="flex-1 px-5"
         showsVerticalScrollIndicator={false}
@@ -33,29 +49,53 @@ export default function Index() {
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-        {moviesLoading ? (
+        {moviesLoading || trendingLoading ? (
           <ActivityIndicator
             size="large"
             color="#0000ff"
-            className="m-10 self-center"
+            className="mt-10 self-center"
           />
-        ) : moviesError ? (
-          <Text className="text-lg text-white">
-            Error: {moviesError?.message}
-          </Text>
+        ) : moviesError || trendingError ? (
+          <Text>Error: {moviesError?.message || trendingError?.message}</Text>
         ) : (
           <View className="flex-1 mt-5">
             <SearchBar
-              onPress={() => router.push("/search")}
+              onPress={() => {
+                router.push("/search");
+              }}
               placeholder="Search for a movie"
             />
+
+            {trendingMovies && (
+              <View className="mt-10">
+                <Text className="text-lg text-white font-bold mb-3">
+                  Trending Movies
+                </Text>
+                <FlatList
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  className="mb-4 mt-3"
+                  data={trendingMovies}
+                  contentContainerStyle={{
+                    gap: 26,
+                  }}
+                  renderItem={({ item, index }) => (
+                    <TrendingCard movie={item} index={index} />
+                  )}
+                  keyExtractor={(item) => item.movie_id.toString()}
+                  ItemSeparatorComponent={() => <View className="w-4" />}
+                />
+              </View>
+            )}
 
             <>
               <Text className="text-lg text-white font-bold mt-5 mb-3">
                 Latest Movies
               </Text>
+
               <FlatList
                 data={movies}
+                renderItem={({ item }) => <MovieCard {...item} />}
                 keyExtractor={(item) => item.id.toString()}
                 numColumns={3}
                 columnWrapperStyle={{
@@ -64,9 +104,6 @@ export default function Index() {
                   paddingRight: 5,
                   marginBottom: 10,
                 }}
-                renderItem={({ item }) => (
-                  <MovieCard {...item}/>
-                )}
                 className="mt-2 pb-32"
                 scrollEnabled={false}
               />
@@ -76,4 +113,6 @@ export default function Index() {
       </ScrollView>
     </View>
   );
-}
+};
+
+export default Index;
